fae391d09b732cfbb355458237295932bf373ead
Kristian Ebdrup <54897@grundfos.com>
2021-04-15 10:49:10 +0200

    Update

--- Modules/Dev/Network/Switch_Reset/Reset-CiscoSwitch.ps1
+++ Modules/Dev/Network/Switch_Reset/Reset-CiscoSwitch.ps1
@@ -2,20 +2,25 @@ if (!$user) { $user = Read-Host -Prompt 'User Creds' }
 if (!$UserPassword) { $UserPassword = Read-Host -Prompt 'Insert Password' }
 
 $Comport = [System.IO.Ports.SerialPort]::getportnames()[-1]
-Write-Host "$Comport" -ForegroundColor Green
-$port = New-Object System.IO.Ports.SerialPort $Comport, 9600, None, 8, one
-$port.open()
-Start-Sleep -Seconds 1
-$port.WriteLine(“$user”)
-Start-Sleep -Seconds 1
-$port.WriteLine(“$UserPassword”)
-Start-Sleep -Seconds 1
-$port.WriteLine('Enable')
-Start-Sleep -Seconds 1
-$port.WriteLine('write erase')
-Start-Sleep -Seconds 1
-$port.WriteLine('y')
-Start-Sleep -Seconds 1
-$port.ReadLine()
-Start-Sleep -Seconds 1
-$port.Close()
+if (($Comport -ne 'COM4') -or ($Comport -ne 'COM5')) {
+    Write-Host "$Comport" -ForegroundColor Green
+    $port = New-Object System.IO.Ports.SerialPort   $Comport, 9600, None, 8, one
+    $port.open()
+    Start-Sleep -Seconds 1
+    $port.WriteLine(“$user”)
+    Start-Sleep -Seconds 1
+    $port.WriteLine(“$UserPassword”)
+    Start-Sleep -Seconds 1
+    $port.WriteLine("Enable")
+    Start-Sleep -Seconds 1
+    $port.WriteLine("write erase")
+    Start-Sleep -Seconds 1
+    $port.WriteLine("y")
+    Start-Sleep -Seconds 1
+    $port.ReadLine()
+    Start-Sleep -Seconds 1
+    $port.Close()
+}
+else {
+    Write-Host "Comport is: $Comport" -ForegroundColor 'Cyan'
+}
