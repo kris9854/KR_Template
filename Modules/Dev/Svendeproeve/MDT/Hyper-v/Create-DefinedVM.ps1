f5af3f9a4ddbc06f7e4203c02a5997258b8e0cd2
Kristian Ebdrup <54897@grundfos.com>
2021-04-22 08:44:03 +0200

    Create-DefinedVM

--- Modules/Dev/Svendeproeve/MDT/Hyper-v/Create-DefinedVM.ps1
+++ Modules/Dev/Svendeproeve/MDT/Hyper-v/Create-DefinedVM.ps1
@@ -52,20 +52,48 @@ function Create-DefinedVM {
         # EnvFileLocation
         [Parameter(Mandatory = $false)]
         [string]
-        $EnvFileLocation = '.\HypervEnv.ps1'
+        $EnvFileLocation = '.\HypervEnv.ps1',
+        # Csvheader Containing the hostnames
+        [Parameter(Mandatory = $false)]
+        [string]
+        $CSVHeader = 'ServerName'
     )
     
     begin {
+        # Variable location
+        $CliCoulour = 'Yellow'
+        # Variable Read count
+        [string]$VMParentDisk = [string]$Global:StandardVMParentDisk
+        [int]$VMGen = [int]$Global:StandardVMGen
+        [int]$ServerVlan = [int]$Global:StandarServerVlan
+        [int]$ClientVlan = [int]$Global:StandardClientVlan
+
+        # Output to PS CLI
+        Write-Host -Object '##########################################' -ForegroundColor $CliCoulour
+        Write-Host -Object "##      VM Parent Disk: $VMParentDisk   ##" -ForegroundColor $CliCoulour
+        Write-Host -Object "##      VM Generation: $VMGen           ##" -ForegroundColor $CliCoulour
+        Write-Host -Object "##      Server Vlan: $ServerVlan        ##" -ForegroundColor $CliCoulour
+        Write-Host -Object "##      Client Vlan: $ClientVlan        ##" -ForegroundColor $CliCoulour
+        Write-Host -Object '##########################################' -ForegroundColor $CliCoulour
+
         if (-not $VMName) {
             Write-Host -Object 'Please provice a name for the new machine, IF you want to create multiple press ANY button: "' -ForegroundColor "$Global:ForegroundColour" -NoNewline
             $VMName = Read-Host -ErrorAction SilentlyContinue
-            if (($VMName -eq $CSV) -or ([string]::IsNullOrEmpty($vmName))) {   
+            if (($VMName -eq $CSV) -or ([string]::IsNullOrEmpty($vmName))) {
+                $CsvImport = (Import-Csv -Path '.\servers.csv' -Encoding Default -Delimiter ';').$CSVHeader
             } 
+        }
+        else {
+            # To reduce code later i will set the csvimport name equal to the VMNAME
+            $CsvImport = $VMName
         }
     }
     
     process {
-        
+        # Device Creation process
+        foreach ($VM in $CsvImport) {
+            
+        }
     }
     
     end {
--- Modules/Dev/Svendeproeve/MDT/Hyper-v/HypervEnv.ps1
+++ Modules/Dev/Svendeproeve/MDT/Hyper-v/HypervEnv.ps1
@@ -1,7 +1,7 @@
 # Env for the vitual machine creation process
 
 [string]$Global:ForegroundColour = 'cyan'
-[string]$Global:VMParentDisk = 'c:\Server\Disk\WIN2019\VM-Parent01.vhdx'
-[int]$Global:VMGen = 2
-[int]$Global:ServerVlan = 10
-[int]$Global:ClientVlan = 20
+[string]$Global:StandardVMParentDisk = 'c:\Server\Disk\WIN2019\VM-Parent01.vhdx'
+[int]$Global:StandardVMGen = 2
+[int]$Global:StandarServerVlan = 10
+[int]$Global:StandardClientVlan = 20
