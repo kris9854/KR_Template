bb83225a28345b1e037eb32e98a9aaa7647e716f
Kristian Ebdrup <54897@grundfos.com>
2021-04-23 10:40:20 +0200

    Update

--- Modules/Dev/Svendeproeve/Hyper-v/HypervEnv.ps1
+++ Modules/Dev/Svendeproeve/Hyper-v/HypervEnv.ps1
@@ -1,6 +1,6 @@
 # Env for the vitual machine creation process
 
-[string]$Global:ForegroundColour = 'cyan'
+[string]$Global:ForegroundColour = 'magenta'
 [string]$Global:StandardVMParentDisk = 'c:\Server\Disk\WIN2019\VM-Parent01.vhdx'
 [int]$Global:StandardVMGen = 2
 [int]$Global:StandarServerVlan = 10
