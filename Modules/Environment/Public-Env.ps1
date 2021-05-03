747bd448ef0d393b4f73aa7e589a3675db6109a5
Kristian Ebdrup <54897@grundfos.com>
2021-05-03 08:06:52 +0200

    Update

---
+++ Modules/Environment/Public-Env.ps1
@@ -0,0 +1,15 @@
+##############################################################
+<#                                                           #
+This is a Public Env file. Do not write sensitive data here. #
+It will be uploaded to your choosen repository               #
+#>                                                           #
+##############################################################
+#
+#
+#
+#Region Colour
+$Global:writehostsuccessfullcolour = 'green'
+$Global:BannerColour = 'cyan'
+$Global:TextColour = 'cyan'
+$Global:ErrorTextColour = 'red'
+#Endregion Colour
