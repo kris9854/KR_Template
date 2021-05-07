403582ef5257fc962c288ae327172a93a8acbdbe
Kristian Ebdrup <54897@grundfos.com>
2021-05-07 14:31:02 +0200

    Update

--- KR_template.ps1
+++ KR_template.ps1
@@ -1,18 +1,20 @@
+$ErrorActionPreference = 'SilentlyContinue'
 Set-Location "$PSScriptRoot"
-$host.ui.RawUI.WindowTitle = 'KR_Template'
+$host.ui.RawUI.WindowTitle = 'Network-PowerAutomation'
 $Host.UI.RawUI.BackgroundColor = 'Black'
 $Host.UI.RawUI.ForegroundColor = 'Gray'
-$Host.PrivateData.FormatAccentColor = Green
-$Host.PrivateData.ErrorAccentColor = Cyan
-$Host.PrivateData.ErrorForegroundColor = Red
-$Host.PrivateData.ErrorBackgroundColor = Black
-$Host.PrivateData.WarningForegroundColor = Yellow
-$Host.PrivateData.WarningBackgroundColor = Black
-$Host.PrivateData.DebugForegroundColor = Yellow
-$Host.PrivateData.DebugBackgroundColor = Black
-$Host.PrivateData.VerboseForegroundColor = Yellow
-$Host.PrivateData.VerboseBackgroundColor = Black
-$Host.PrivateData.ProgressForegroundColor = Black
-$Host.PrivateData.ProgressBackgroundColor = Yellow
+$Host.PrivateData.FormatAccentColor = 'Green'
+$Host.PrivateData.ErrorAccentColor = 'Cyan'
+$Host.PrivateData.ErrorForegroundColor = 'Red'
+$Host.PrivateData.ErrorBackgroundColor = 'Black'
+$Host.PrivateData.WarningForegroundColor = 'Yellow'
+$Host.PrivateData.WarningBackgroundColor = 'Black'
+$Host.PrivateData.DebugForegroundColor = 'Yellow'
+$Host.PrivateData.DebugBackgroundColor = 'Black'
+$Host.PrivateData.VerboseForegroundColor = 'Yellow'
+$Host.PrivateData.VerboseBackgroundColor = 'Black'
+$Host.PrivateData.ProgressForegroundColor = 'Black'
+$Host.PrivateData.ProgressBackgroundColor = 'Yellow'
+$ErrorActionPreference = 'Continue'
 Clear-Host
-pwsh.exe -NoExit -command ". .\MainScript.ps1"
+pwsh.exe -NoExit -ExecutionPolicy Bypass -command ". .\MainScript.ps1"
