2c9702e28b6faa1ba62a67f4bc0be602dbb15c95
kris9854 <kris9854@gmial.com>
2021-05-21 10:00:16 +0200

    update

--- Modules/Svendeproeve/Hyper-v/Create-DefinedVM.ps1
+++ Modules/Svendeproeve/Hyper-v/Create-DefinedVM.ps1
@@ -181,24 +181,10 @@ function GLobalVars {
     [int]$Global:StandardVMGen = 2
     [int]$Global:StandarServerVlan = 4
     [int]$Global:StandardClientVlan = 6
-    ##############################################################
-    <#                                                           #
-This is a Public Env file. Do not write sensitive data here. #
-It will be uploaded to your choosen repository               #
-#>                                                           #
-    ##############################################################
-    #
-    #
-    #
-    #Region Colour
     $Global:writehostsuccessfullcolour = 'green'
     $Global:BannerColour = 'cyan'
     $Global:TextColour = 'cyan'
     $Global:ErrorTextColour = 'red'
-    #Endregion Colour
-
-    ##
-    #Svendeproeve
     [string]$Global:ForegroundColour = 'cyan'
     [string]$global:TxtColour = 'Cyan';
     [string]$global:ConfirmColour = 'yellow';
