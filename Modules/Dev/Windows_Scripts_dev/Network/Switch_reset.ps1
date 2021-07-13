793e9298ac3c03fe89b4b730ab30d1775837bc5b
Kristian <kris9854@gmail.com>
2021-07-13 09:31:10 +0200

    Switch_reset ready for test

--- Modules/Dev/Windows_Scripts_dev/Network/Switch_reset.ps1
+++ Modules/Dev/Windows_Scripts_dev/Network/Switch_reset.ps1
@@ -1,10 +1,20 @@
 function Reset-Switch {
     [CmdletBinding()]
     param (
+        # User
+        [Parameter(Mandatory = $true)]
+        [string]
+        $User,
+
+        # User
+        [Parameter(Mandatory = $true)]
+        [string]
+        $UserPassword,
+
         # COMPORT
         [Parameter(Mandatory = $false)]
         [string]
-        $COMPORT = 'COM5',
+        $COMPORT,
 
         # DeviceName Uses Network device types
         [Parameter(Mandatory = $false)]
@@ -25,21 +35,44 @@ function Reset-Switch {
         # Path to Text Fil for Cisco device
         [Parameter(Mandatory = $false)]
         [string]
-        $Cisco_IOS_Reset = "$PSScriptRoot\Switch_Reset_Commands\Cisco_IOS_Reset.txt"
+        $Cisco_IOS_Reset = "$PSScriptRoot\Switch_Reset_Commands\Cisco_IOS_Reset.txt",
+
+
     )
     
     begin {
+        $lookupTable = @{
+            '{{Password}}' = "$UserPassword"
+            '{{User}}'     = "$User"
+        }
+
         # Create the commands based on the device type
         switch ($ModelType) {
             Cisco.IOS {
                 [bool]$ModelTypeSet = $true
-                [array]$ResetCommands = (Get-Content -Path "$Cisco_IOS_Reset") 
+                [array]$ResetCommands = (Get-Content -Path "$Cisco_IOS_Reset") | ForEach-Object {
+                    $line = $_
+                    $lookupTable.GetEnumerator() | ForEach-Object {
+                        if ($line -match $_.Key) {
+                            $line = $line -replace $_.name, $_.Value
+                        }
+                    }
+                    $line
+                }  
                 Break;
             }
             Comware7 {
                 [bool]$ModelTypeSet = $true
 
-                [array]$ResetCommands = (Get-Content -Path "$Comware7_Reset") 
+                [array]$ResetCommands = (Get-Content -Path "$Comware7_Reset") | ForEach-Object {
+                    $line = $_
+                    $lookupTable.GetEnumerator() | ForEach-Object {
+                        if ($line -match $_.Key) {
+                            $line = $line -replace $_.name, $_.Value
+                        }
+                    }
+                    $line
+                } 
                 Break;
             }
             Default {
@@ -51,27 +84,30 @@ function Reset-Switch {
             return("Please provide a Network Device")
         }
 
+        if (!$COMPORT) {
+            $COMPORT = [System.IO.Ports.SerialPort]::getportnames()[1]
+        }
 
-        $Port_Con = New-Object System.IO.Ports.SerialPort "$COMPORT", 9600, None, 8, one
+        $Port = New-Object System.IO.Ports.SerialPort "$COMPORT", 9600, None, 8, one
     }
     
     process {
         [array]$PortLineRead
-        $Port_Con.open()
-        #$Port_Con.ReadLine() # Deletes 
+        $port.ReadTimeout = 9000
+        $Port.open()
         foreach ($Line in $ResetCommands) {
             Write-Verbose -Message "Processing: $Line"
 
             Start-Sleep -Seconds 3
-            $Port_Con.WriteLine("$Line")
+            $Port.WriteLine("$Line")
             Start-Sleep -Seconds 3
         }
-        $PortLineRead = $Port_Con.ReadLine()
+        $PortLineRead = $Port.ReadExisting()
         Write-Debug -Message "$PortLineRead"
     }
     
     end {
-        $Port_Con.Close()
+        $Port.Close()
         Return("$PortLineRead")
     }
 }
